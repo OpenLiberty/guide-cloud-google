360d14ed2cb0aec07986e83682cf0f95c7ee7bc3
Austin Seto <austin.seto@ibm.com>
2020-11-30 16:18:11 -0500

    typo fix

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -7,7 +7,7 @@ curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s htt
 chmod +x kubectl
 sudo ln -s $(pwd)/kubectl /usr/local/bin/kubectl
 wget https://github.com/kubernetes/minikube/releases/download/v0.28.2/minikube-linux-amd64 -q -O minikube
-chmod +x minikube]
+chmod +x minikube
 
 sudo apt-get update -y
 sudo apt-get install -y conntrack
