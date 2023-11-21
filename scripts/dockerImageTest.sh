1f984f9457182c45a013b8061a7d3392ec6a3a96
Gilbert Kwan <gkwan@ca.ibm.com>
2023-11-21 17:44:15 -0500

    Change to use image from cp.stg.icr.io (#62)

--- scripts/dockerImageTest.sh
+++ scripts/dockerImageTest.sh
@@ -13,9 +13,9 @@ echo "Test latest OpenLiberty Docker image"
 sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/""$DATE""/""$DRIVER""</runtimeUrl></install></configuration>" system/pom.xml inventory/pom.xml
 cat system/pom.xml inventory/pom.xml
 
-sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM openliberty/daily:latest;g" inventory/Dockerfile system/Dockerfile
+sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM cp.stg.icr.io/cp/olc/open-liberty-daily:full-java11-openj9-ubi;g" inventory/Dockerfile system/Dockerfile
 cat inventory/Dockerfile system/Dockerfile
 
-docker pull -q "openliberty/daily:latest"
+docker pull -q "cp.stg.icr.io/cp/olc/open-liberty-daily:full-java11-openj9-ubi"
 
 sudo -u runner ../scripts/testApp.sh
